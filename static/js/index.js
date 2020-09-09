63ab9ba3ac302c433d4d8f73f6a269224b654bd2
ruberchiles <36668479+ruberchiles@users.noreply.github.com>
2020-09-08 21:09:40 -0500

    Add files via upload

--- static/js/index.js
+++ static/js/index.js
@@ -3,7 +3,7 @@
 function LED1_On() {
 	
 	console.log("suma");
-        message = new Paho.MQTT.Message("suma1");
+        message = new Paho.MQTT.Message("suma");
         message.destinationName = "ruberchiles@hotmail.es/test1";
         client.send(message);
         
@@ -12,7 +12,7 @@ function LED1_On() {
 function LED1_Off(){	
 	
 	console.log("resta");
-        message = new Paho.MQTT.Message("resta1")
+        message = new Paho.MQTT.Message("resta")
         message.destinationName = "ruberchiles@hotmail.es/test1";
         client.send(message);
 }
